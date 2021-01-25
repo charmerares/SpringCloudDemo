f4076b5b49c0d72c5e71c52608b8647c6459ba8f
Iris <royalvald@sina.com>
2021-01-25 20:03:34 +0800

    add config and provider

---
+++ springconfigdemo/src/main/java/com/iris/springconfigdemo/SpringconfigdemoApplication.java
@@ -0,0 +1,17 @@
+package com.iris.springconfigdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class SpringconfigdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringconfigdemoApplication.class, args);
+    }
+
+}
---
+++ springconfigdemo/src/test/java/com/iris/springconfigdemo/SpringconfigdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.iris.springconfigdemo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringconfigdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
